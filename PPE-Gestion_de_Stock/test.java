83cb7afa1c0039f446e3107c270224f9c969825a
julien <julien@pc-desh>
2016-04-28 13:40:39 +0200

    le commit total

--- PPE-Gestion_de_Stock/test.java
+++ PPE-Gestion_de_Stock/test.java
@@ -1,10 +1,12 @@
 
 public class test {
 
-	
+
 	// รงa marche ou paaas ->>> ca marche xd
 }
 
  // black jesus
 // excellent
+
+// une relation R est dite d'ordre si elle est เ la fois reflexive, anti symetrique et transitive
 
